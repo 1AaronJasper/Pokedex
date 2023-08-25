5c53b5d42ff655ad77b83b381cbc44a0b57017e8
1AaronJasper <aaronjasper94@gmail.com>
2023-08-25 16:26:37 -0500

    new

--- server.js
+++ server.js
@@ -1,75 +1,90 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
 const methodOverride = require('method-override')
-const app = express()
-const PORT = 6000
-let Pokemon = require('./models/pokemon.js')
+const morgan = require('morgan')
+const app = express();
+const port = 8000;
 const expressEjsLayouts = require('express-ejs-layouts')
 
-//MIDDLEWARE
+const Pokemon = require('./models/pokemon.js')
+
 app.use(morgan('tiny'))
 app.set('view engine', 'ejs')
 app.use(expressEjsLayouts)
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended: true}))
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-//index
-app.get("/pokemon", (req, res) => {
-    res.render('index.ejs', { Pokemon });
-    });
+
+
+// INDUCES 
+
+// index
+app.get('/pokemon', (req, res) => {
+    res.render('index.ejs', {
+        Pokemon
+    })
+})
 
 // new
-
-app.get("/pokemon/new", (req, res) => {
-    res.render("new.ejs")
-});
-
-// DELETE
-app.delete("/pokemon/:index", (req, res)=>{
-    Pokemon.splice(req.params.index, 1)
-    res.redirect("/pokemon")
-});
-
-//UPDATE
-app.put("/pokemon/:index", (req, res)=>{
-    let thePoke=Pokemon[req.params.index]
-    thePoke.name = req.body.name
-    thePoke.stats.hp = req.body.hp
-    thePoke.stats.attack = req.body.attack
-    thePoke.stats.defense = req.body.defense
-    thePoke.type = req.body.type.split(', ')
-    res.redirect("/pokemon")
-})
-//CREATE
-app.post("/pokemon", (req, res)=>{
-    let newPokie = {}
-    newPokie.id = parseInt(Pokemon[Pokemon.length-1].id)+1
-    newPokie.name = req.body.name
-    newPokie.stats = {}
-    newPokie.stats.hp = req.body.hp
-    newPokie.stats.attack = req.body.attack
-    newPokie.stats.defense = req.body.defense
-    newPokie.type = req.body.type.split(', ')
-    Pokemon.push(newPokie)
-    res.redirect("/pokemon")
-});
-
-//EDIT
-app.get("/pokemon/:index/edit", (req, res)=>{
-    let pokeId = req.params.index
-    res.render('pokemon/edit.ejs', {
-        poke: Pokemon[pokeId],
-        index: pokeId
+app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs', {
+        Pokemon
     })
-});
 
-//SHOW
-app.get("/pokemon/:index", (req, res)=>{
-    res.render("pokemon/show.ejs", {
+
+})
+
+// delete
+app.delete('/pokemon/:index', (req, res) => {
+    Pokemon.splice(req.params.index, 1)
+    res.redirect('/pokemon')
+})
+
+// update
+app.put('/pokemon/:index', (req, res) => {
+    let firstPoke = Pokemon[req.params.index];
+    firstPoke.name = req.body.name;
+    firstPoke.stats.hp = req.body.hp;
+    firstPoke.stats.attack = req.body.attack;
+    firstPoke.stats.defense = req.body.defense;
+    originalPoke.type = req.body.type.split(' ,  ')
+    res.redirect('/pokemon')
+})
+
+// create
+app.post('/pokemon', (req, res) => {
+    let newPokemon = {};
+    newPokemon.id = parseInt(Pokemon[Pokemon.length-1].id)+1
+    newPokemon.name = req.body.name;
+    newPokemon.stats = {}
+    newPokemon.stats.hp = req.body.hp
+    newPokemon.stats.attack = req.body.attack
+    newPokemon.stats.defense = req.body.defense
+    newPokemon.type = req.body.type.split(' ,  ')
+    Pokemon.push(newPokemon)
+    res.redirect('/pokemon')
+})
+
+// edit
+app.get('/pokemon/:index/edit', (req, res) => {
+    let pokemonId = req.params.index;
+    res.render('edit.ejs', {
+        pokemon: Pokemon[pokemonId],
+        index: pokemonId
+    })
+})
+
+
+// show
+app.get('/pokemon/:index', (req, res) => {
+    res.render('show.ejs', {
         poke: Pokemon[req.params.index]
     })
-});
+})
 
 
-app.listen(PORT, () => console.log(`can't dock on this port ${PORT}`));
+
+
+app.listen(port, () => {
+    console.log("listening in on port: ", port)
+})
